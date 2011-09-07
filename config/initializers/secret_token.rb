cd980120af6f8493fa622b2327749d8eba6cfce0
Adam Holt <ads.noob@gmail.com>
2011-09-07 11:10:02 +0100

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SphinxChefTest::Application.config.secret_token = '31aebd74d9e44fb3b8e78d8c11c7b4844471829ac5c0354b7da663cb60fa21f8f26997bd837d0f802b8f71b853cd437429c19256c158ad36c1f3f01f566405fd'
